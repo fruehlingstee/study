50fc4eebb50005e5e746b5f7d1ae9ea19fd8d57b
rin2426 <pazrin0493@Gmail.com>
2026-05-20 13:30:41 +0900

    add

---
+++ js/js/script.js
@@ -0,0 +1,18 @@
+// let 変数定義
+let name ="name";
+
+// 再定義時はlet無し
+name = "new name";
+
+// console.log 出力
+console.log(name);
+
+// const (const 定数名 = "値") 定数定義
+const birthyear = 2006;
+console.log(birthyear);
+
+// if, slse if, else 条件分岐
+// &&, || 論理演算子
+// switch 条件分岐  case 条件式 break; default: デフォルトの処理 break;
+// for (let 変数定義; 条件式; 変数の更新(++(+1) --(-1))) {処理｝)
+// `${}` 文字列の中で変数を展開
---
+++ ruby/base.rb
@@ -0,0 +1,5 @@
+puts "Hello, World!"
+# if elsif else end
+# and = &&
+# or = ||
+# #{} 文字列の中で変数を展開
